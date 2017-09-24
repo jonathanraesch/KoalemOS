41106cf0242dbda88411d527525e9bcf2e90c99b
Jonathan Räsch <jonathan.raesch@gmx.de>
2017-09-25 01:20:39 +0200

    Add minimal paging support
    Set up paging structures required to load the 64-bit kernel:
     - identity map boot section memory
     - map kernel memory starting at 0xFF0000000000
     - load base address of pml4 into last pml4 entry to simplify paging
     - load cr3 with base address of pml4

---
+++ boot/paging.c
@@ -0,0 +1,49 @@
+#include "paging.h"
+#include <stdint.h>
+
+
+/* linker symbols */
+extern uint8_t _boot_begin_addr;
+extern uint8_t _boot_end_addr;
+extern uint8_t _kernel_begin_physaddr;
+extern uint8_t _kernel_end_physaddr;
+
+
+/* neither kernel nor boot code can exceed their 1GB aligned memory region
+   if their base address is 1GB aligned, the maximum size is 1GB */
+static uint64_t pml4[512];
+static uint64_t boot_pdpt[512];
+static uint64_t boot_pd[512];
+static uint64_t boot_pts[512][512];
+static uint64_t kernel_pdpt[512];
+static uint64_t kernel_pd[512];
+static uint64_t kernel_pts[512][512];
+
+
+void* paging_set_up_boot_mapping() {
+	pml4[0] = ((uintptr_t)boot_pdpt) | PAGING_FLAG_PRESENT | PAGING_FLAG_READ_WRITE;
+	boot_pdpt[PAGING_PDPT_OFFSET((uintptr_t)(&_boot_begin_addr))] = ((uintptr_t)boot_pd) | PAGING_FLAG_PRESENT | PAGING_FLAG_READ_WRITE;
+	for(int i = 0; i < 512; i++) {
+		boot_pd[i] = ((uintptr_t)(boot_pts[i])) | PAGING_FLAG_PRESENT | PAGING_FLAG_READ_WRITE;
+	}
+	for(int i = 0; i < 512; i++) {
+		for(int j = 0; j < 512; j++) {
+			kernel_pts[i][j] = (((uintptr_t)(&_boot_begin_addr) & -30) + 0x200000*i + 0x1000*j) | PAGING_FLAG_PRESENT | PAGING_FLAG_READ_WRITE;
+		}
+	}
+
+	pml4[PAGING_PML4_OFFSET(KERNEL_LINADDR)] = ((uintptr_t)kernel_pdpt) | PAGING_FLAG_PRESENT | PAGING_FLAG_READ_WRITE;
+	kernel_pdpt[PAGING_PDPT_OFFSET(KERNEL_LINADDR)] = ((uintptr_t)kernel_pd) | PAGING_FLAG_PRESENT | PAGING_FLAG_READ_WRITE;
+	for(int i = 0; i < 512; i++) {
+		kernel_pd[i] = ((uintptr_t)(kernel_pts[i])) | PAGING_FLAG_PRESENT | PAGING_FLAG_READ_WRITE;
+	}
+	for(int i = 0; i < 512; i++) {
+		for(int j = 0; j < 512; j++) {
+			kernel_pts[i][j] = ((uintptr_t)(&_kernel_begin_physaddr) + 0x200000*i + 0x1000*j) | PAGING_FLAG_PRESENT | PAGING_FLAG_READ_WRITE;
+		}
+	}
+
+	pml4[511] = ((uintptr_t)pml4) | PAGING_FLAG_PRESENT | PAGING_FLAG_READ_WRITE;
+
+	return pml4;
+}
