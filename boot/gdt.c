b902d0f9829b2f6911dfafbc3f72f5861179ea15
Jonathan Räsch <jonathan.raesch@gmx.de>
2017-09-25 23:45:20 +0200

    Rewrite 32bit GDT in C

---
+++ boot/gdt.c
@@ -0,0 +1,28 @@
+#include "gdt.h"
+
+
+const uint64_t boot_gdt32[3] = {
+	0,
+	GDT_SEGMENT_DESCRIPTOR(
+		0x00000000,
+		0x000FFFFF,
+		GDT_SEGMENT_TYPE_CODE | GDT_SEGMENT_TYPE_EXECUTE_READ,
+		0,
+		GDT_SEGMENT_FLAG_S_TYPE | GDT_SEGMENT_FLAG_PRESENT | GDT_SEGMENT_FLAG_OPERAND_SIZE | GDT_SEGMENT_FLAG_GRANULARITY
+	),
+	GDT_SEGMENT_DESCRIPTOR(
+		0x00000000,
+		0x000FFFFF,
+		GDT_SEGMENT_TYPE_DATA | GDT_SEGMENT_TYPE_READ_WRITE,
+		0,
+		GDT_SEGMENT_FLAG_S_TYPE | GDT_SEGMENT_FLAG_PRESENT | GDT_SEGMENT_FLAG_OPERAND_SIZE | GDT_SEGMENT_FLAG_GRANULARITY
+	)
+};
+
+static gdtr_t boot_gdtr32 = {0, 0};
+
+gdtr_t* boot_set_up_gdt32() {
+	boot_gdtr32.base_address = (uint32_t)boot_gdt32;	
+	boot_gdtr32.limit = sizeof(boot_gdt32);
+	return &boot_gdtr32;
+}
