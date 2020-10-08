e32d95ded90310117870ab038998c059c13e30c9
Jonathan Räsch <raesch.jonathan@gmail.com>
2020-10-08 18:20:38 +0200

    libc: fix indentation and whitespace in stdlib.c

--- src/libc/stdlib.c
+++ src/libc/stdlib.c
@@ -4,11 +4,11 @@
 #ifdef __klibc__
 #include "kernel/memory.h"
 
-	void* malloc(size_t size) {
-		return kmalloc(size);
-	}
-	void free(void* ptr) {
-		kfree(ptr);
-	}
+void* malloc(size_t size) {
+	return kmalloc(size);
+}
+void free(void* ptr) {
+	kfree(ptr);
+}
 
 #endif
