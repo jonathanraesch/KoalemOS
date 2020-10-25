847a909bfc4e9582d1f24c165814c4a49170a33e
Jonathan Räsch <raesch.jonathan@gmail.com>
2020-10-25 21:16:21 +0100

    Implement kernel TSS

--- src/kernel/kernel_start.s
+++ src/kernel/kernel_start.s
@@ -1,17 +1,35 @@
 
 .section .data
+
 kernel_gdt:
+# GDT[0]: zero descriptor
 .quad 0
+# GDT[1]: code segment descriptor
 .quad 0x209A0000000000
+# GDT[2]: data segment descriptor
 .quad 0x920000000000
+# GDT[3]: data segment descriptor
+.fill 16, 1, 0
+kernel_gdt_end:
 kernel_gdtr:
-.short 23
+.short kernel_gdt_end-kernel_gdt-1
 .quad kernel_gdt
 kernel_gdt_jmp_target:
 .quad kernel_gdt_loaded
 .short 0x08
 
+.align 8
+kernel_tss:
+.4byte 0
+.8byte istack_0_bottom
+.8byte istack_1_bottom
+.8byte istack_2_bottom
+.fill 74, 1, 0
+.4byte 104
+
+
 .section .bss
+
 .skip 0x100000
 stack_bottom:
 
@@ -30,6 +48,20 @@ _kernel_start:
 	push rcx	#	fb_info			[rbp-16]
 	push r8		#	acpi_x_r_sdt	[rbp-24]
 
+	# initialize TSS descriptor
+	lea rbx, kernel_gdt+24[rip]
+	mov word ptr [rbx], 103
+	lea rdx, kernel_tss[rip]
+	mov rax, 0xFFFFFF
+	and rax, rdx
+	mov [rbx+2], rax
+	mov rax, 0xFFFFFFFFFF000000
+	and rax, rdx
+	shr rax, 24
+	mov [rbx+7], rax
+	mov ax, 0x89
+	mov [rbx+5], ax
+
 	# load gdt
 	lea rax, kernel_gdt_jmp_target[rip]
 	lea rbx, kernel_gdtr[rip]
@@ -43,6 +75,9 @@ kernel_gdt_loaded:
 	mov es, ax
 	mov fs, ax
 	mov gs, ax
+	# load TSS
+	mov ax, 24
+	ltr ax
 
 	call setup_idt
 	sti
