22a89fd1bfcaab214a093cb9af5255dada2f9a0d
Jonathan Räsch <raesch.jonathan@gmail.com>
2021-03-10 18:42:48 +0100

    Check for PCI initialization success

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -14,7 +14,9 @@ void kmain(gop_framebuffer_info* gop_fb_info, void* acpi_x_r_sdt) {
 	init_graphics(gop_fb_info, 20);
 	fill_screen(0.0, 0.0, 0.0);
 	init_acpi(acpi_x_r_sdt);
-	init_pci();
+	if(init_pci()) {
+		kernel_panic();
+	}
 	init_apic();
 
 	start_apic_timer(50000000, 7, true, timer_tick);
