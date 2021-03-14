e9993a7743bd0fc8a590533607b81250801ab9f3
Jonathan Räsch <raesch.jonathan@gmail.com>
2021-03-14 23:48:20 +0100

    Print error message in kernel_panic

--- src/kernel/kernel.s
+++ src/kernel/kernel.s
@@ -4,7 +4,14 @@
 .global kernel_panic
 kernel_panic:
 	cli
-	mov rbx, 0
-	div rbx
+	lea rdi, panic_str[rip]
+	call print_str
 	_kernel_panic_loop:
+	hlt
 	jmp _kernel_panic_loop
+
+
+.section .rodata
+
+panic_str:
+.string32 "\n--- kernel panic! ---\n"
