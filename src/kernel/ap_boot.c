07b884ada702bee36c161723d30ecd4cee395c18
Jonathan Räsch <raesch.jonathan@gmail.com>
2021-03-16 00:17:18 +0100

    Fix page mapping for AP bootstrap

--- src/kernel/ap_boot.c
+++ src/kernel/ap_boot.c
@@ -13,7 +13,7 @@ void* ap_boot_paddr = 0;
 void boot_aps() {
 	int pages = ap_boot_image_size % 0x1000 ? ap_boot_image_size / 0x1000 + 1 : ap_boot_image_size % 0x1000;
 	for(int i = 0; i < pages; i++) {
-		map_page((void*)((uintptr_t)ap_boot_paddr + 0x1000*pages), (void*)((uintptr_t)ap_boot_paddr + 0x1000*pages), PAGING_FLAG_READ_WRITE);
+		map_page((void*)((uintptr_t)ap_boot_paddr + 0x1000*i), (void*)((uintptr_t)ap_boot_paddr + 0x1000*i), PAGING_FLAG_READ_WRITE);
 	}
 	memcpy(ap_boot_paddr, &ap_boot_image, ap_boot_image_size);
 
