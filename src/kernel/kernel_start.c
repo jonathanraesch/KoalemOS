aaf02a63a9f032c2099ce864bafbd1f540fada68
Jonathan Räsch <raesch.jonathan@gmail.com>
2021-03-18 16:20:17 +0100

    Move memory management init into BSP init

--- src/kernel/kernel_start.c
+++ src/kernel/kernel_start.c
@@ -21,7 +21,10 @@ void* __get_kernel_sp() {
 	return &kernel_stack[0x2000];
 }
 
-void __kernel_bsp_init() {
+void __kernel_bsp_init(boot_info* bi_ptr) {
+	boot_inf = *bi_ptr; // do not use bi_ptr after init_memory_management!
+	init_memory_management(&boot_inf.mmap_data);
+	kernel_post_init_check();
 	init_graphics(&boot_inf.gop_fb_info, 20);
 	fill_screen(0.0, 0.0, 0.0);
 	init_acpi(boot_inf.acpi_x_r_sdt);
@@ -31,15 +34,13 @@ void __kernel_bsp_init() {
 }
 
 void __kernel_init(boot_info* bi_ptr) {
-	boot_inf = *bi_ptr;	// do not use bi_ptr after init_memory_management!
 	init_gdt();
 	setup_idt();
-	init_memory_management(&boot_inf.mmap_data);
-	kernel_post_init_check();
+
+	__kernel_bsp_init(bi_ptr);
+
 	init_apic(boot_inf.tsc_freq_hz);
 	uint16_t ap_count = boot_aps();
-
-	__kernel_bsp_init();
 
 	for(int i = 0; i < ap_count; i++) {
 		print_str(U"AP booted\n");
